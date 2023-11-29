84f09fd3187026e45e84a33c1941d87c85a1c334
Jim Ebbelaar <jimebbelaar@Jims-Mac-Studio.local>
2023-11-29 11:12:07 +0100

    color scroll

---
+++ dark-mode-scroll.js
@@ -0,0 +1,111 @@
+function colorModeToggle() {
+    function attr(defaultVal, attrVal) {
+      const defaultValType = typeof defaultVal;
+      if (typeof attrVal !== "string" || attrVal.trim() === "") return defaultVal;
+      if (attrVal === "true" && defaultValType === "boolean") return true;
+      if (attrVal === "false" && defaultValType === "boolean") return false;
+      if (isNaN(attrVal) && defaultValType === "string") return attrVal;
+      if (!isNaN(attrVal) && defaultValType === "number") return +attrVal;
+      return defaultVal;
+    }
+  
+    const htmlElement = document.documentElement;
+    const computed = getComputedStyle(htmlElement);
+    
+    const scriptTag = document.querySelector("[tr-color-vars]");
+    if (!scriptTag) {
+      console.warn("Script tag with tr-color-vars attribute not found");
+      return;
+    }
+  
+    let colorModeDuration = attr(0.5, scriptTag.getAttribute("duration"));
+    let colorModeEase = attr("power1.out", scriptTag.getAttribute("ease"));
+  
+    const cssVariables = scriptTag.getAttribute("tr-color-vars");
+    if (!cssVariables.length) {
+      console.warn("Value of tr-color-vars attribute not found");
+      return;
+    }
+  
+    let lightColors = {};
+    let darkColors = {};
+    cssVariables.split(",").forEach(function (item) {
+      let lightValue = computed.getPropertyValue(`--color--${item}`);
+      let darkValue = computed.getPropertyValue(`--dark--${item}`);
+      if (lightValue.length) {
+        if (!darkValue.length) darkValue = lightValue;
+        lightColors[`--color--${item}`] = lightValue;
+        darkColors[`--color--${item}`] = darkValue;
+      }
+    });
+  
+    if (!Object.keys(lightColors).length) {
+      console.warn("No variables found matching tr-color-vars attribute value");
+      return;
+    }
+  
+    function setColors(colorObject, animate) {
+      if (typeof gsap !== "undefined" && animate) {
+        gsap.to(htmlElement, {
+          ...colorObject,
+          duration: colorModeDuration,
+          ease: colorModeEase,
+        });
+      } else {
+        Object.keys(colorObject).forEach(function (key) {
+          htmlElement.style.setProperty(key, colorObject[key]);
+        });
+      }
+    }
+  
+    function goDark(dark, animate, ignoreLocalStorage = false) {
+      if (!ignoreLocalStorage) {
+        localStorage.setItem("dark-mode", dark ? "true" : "false");
+      }
+      if (dark) {
+        htmlElement.classList.add("dark-mode");
+        setColors(darkColors, animate);
+      } else {
+        htmlElement.classList.remove("dark-mode");
+        setColors(lightColors, animate);
+      }
+    }
+  
+    function checkPreference(e) {
+      goDark(e.matches, false);
+    }
+    const colorPreference = window.matchMedia("(prefers-color-scheme: dark)");
+    colorPreference.addListener(checkPreference);
+  
+    let storagePreference = localStorage.getItem("dark-mode");
+    if (storagePreference !== null) {
+      storagePreference === "true" ? goDark(true, false) : goDark(false, false);
+    } else {
+      checkPreference(colorPreference);
+    }
+  
+    // Intersection Observer
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+          goDark(prefersDarkMode, true, true);
+        } else {
+          const prefersLightMode = window.matchMedia("(prefers-color-scheme: light)").matches;
+          goDark(!prefersLightMode, true, true);
+        }
+      });
+    }, {
+      root: null, // observing for viewport
+      rootMargin: '0px',
+      threshold: 0.5 // adjust as needed
+    });
+  
+    // Start observing
+    document.querySelectorAll('[tr-color-scroll-toggle]').forEach(el => {
+      observer.observe(el);
+    });
+  }
+  
+  colorModeToggle();
+  
