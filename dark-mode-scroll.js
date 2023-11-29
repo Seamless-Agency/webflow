53c63634e0b8af87a656ae94a4d8e0b309e52370
Jim Ebbelaar <jimebbelaar@Jims-Mac-Studio.local>
2023-11-29 11:45:48 +0100

    updated scroll gsap trigger

--- dark-mode-scroll.js
+++ dark-mode-scroll.js
@@ -1,111 +1,115 @@
-function colorModeToggle() {
-    function attr(defaultVal, attrVal) {
-      const defaultValType = typeof defaultVal;
-      if (typeof attrVal !== "string" || attrVal.trim() === "") return defaultVal;
-      if (attrVal === "true" && defaultValType === "boolean") return true;
-      if (attrVal === "false" && defaultValType === "boolean") return false;
-      if (isNaN(attrVal) && defaultValType === "string") return attrVal;
-      if (!isNaN(attrVal) && defaultValType === "number") return +attrVal;
-      return defaultVal;
+function colorModeScrollToggle() {
+  function attr(defaultVal, attrVal) {
+    const defaultValType = typeof defaultVal;
+    if (typeof attrVal !== "string" || attrVal.trim() === "") return defaultVal;
+    if (attrVal === "true" && defaultValType === "boolean") return true;
+    if (attrVal === "false" && defaultValType === "boolean") return false;
+    if (isNaN(attrVal) && defaultValType === "string") return attrVal;
+    if (!isNaN(attrVal) && defaultValType === "number") return +attrVal;
+    return defaultVal;
+  }
+
+  const htmlElement = document.documentElement;
+  const computed = getComputedStyle(htmlElement);
+  let toggleEl;
+  let togglePressed = "false";
+
+  const scriptTag = document.querySelector("[tr-color-vars]");
+  if (!scriptTag) {
+    console.warn("Script tag with tr-color-vars attribute not found");
+    return;
+  }
+
+  let colorModeDuration = attr(0.5, scriptTag.getAttribute("duration"));
+  let colorModeEase = attr("power1.out", scriptTag.getAttribute("ease"));
+
+  const cssVariables = scriptTag.getAttribute("tr-color-vars");
+  if (!cssVariables.length) {
+    console.warn("Value of tr-color-vars attribute not found");
+    return;
+  }
+
+  let lightColors = {};
+  let darkColors = {};
+  cssVariables.split(",").forEach(function (item) {
+    let lightValue = computed.getPropertyValue(`--color--${item}`);
+    let darkValue = computed.getPropertyValue(`--dark--${item}`);
+    if (lightValue.length) {
+      if (!darkValue.length) darkValue = lightValue;
+      lightColors[`--color--${item}`] = lightValue;
+      darkColors[`--color--${item}`] = darkValue;
     }
-  
-    const htmlElement = document.documentElement;
-    const computed = getComputedStyle(htmlElement);
-    
-    const scriptTag = document.querySelector("[tr-color-vars]");
-    if (!scriptTag) {
-      console.warn("Script tag with tr-color-vars attribute not found");
-      return;
-    }
-  
-    let colorModeDuration = attr(0.5, scriptTag.getAttribute("duration"));
-    let colorModeEase = attr("power1.out", scriptTag.getAttribute("ease"));
-  
-    const cssVariables = scriptTag.getAttribute("tr-color-vars");
-    if (!cssVariables.length) {
-      console.warn("Value of tr-color-vars attribute not found");
-      return;
-    }
-  
-    let lightColors = {};
-    let darkColors = {};
-    cssVariables.split(",").forEach(function (item) {
-      let lightValue = computed.getPropertyValue(`--color--${item}`);
-      let darkValue = computed.getPropertyValue(`--dark--${item}`);
-      if (lightValue.length) {
-        if (!darkValue.length) darkValue = lightValue;
-        lightColors[`--color--${item}`] = lightValue;
-        darkColors[`--color--${item}`] = darkValue;
-      }
-    });
-  
-    if (!Object.keys(lightColors).length) {
-      console.warn("No variables found matching tr-color-vars attribute value");
-      return;
-    }
-  
-    function setColors(colorObject, animate) {
-      if (typeof gsap !== "undefined" && animate) {
-        gsap.to(htmlElement, {
-          ...colorObject,
-          duration: colorModeDuration,
-          ease: colorModeEase,
-        });
-      } else {
-        Object.keys(colorObject).forEach(function (key) {
-          htmlElement.style.setProperty(key, colorObject[key]);
-        });
-      }
-    }
-  
-    function goDark(dark, animate, ignoreLocalStorage = false) {
-      if (!ignoreLocalStorage) {
-        localStorage.setItem("dark-mode", dark ? "true" : "false");
-      }
-      if (dark) {
-        htmlElement.classList.add("dark-mode");
-        setColors(darkColors, animate);
-      } else {
-        htmlElement.classList.remove("dark-mode");
-        setColors(lightColors, animate);
-      }
-    }
-  
-    function checkPreference(e) {
-      goDark(e.matches, false);
-    }
-    const colorPreference = window.matchMedia("(prefers-color-scheme: dark)");
-    colorPreference.addListener(checkPreference);
-  
-    let storagePreference = localStorage.getItem("dark-mode");
-    if (storagePreference !== null) {
-      storagePreference === "true" ? goDark(true, false) : goDark(false, false);
-    } else {
-      checkPreference(colorPreference);
-    }
-  
-    // Intersection Observer
-    const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
-          goDark(prefersDarkMode, true, true);
-        } else {
-          const prefersLightMode = window.matchMedia("(prefers-color-scheme: light)").matches;
-          goDark(!prefersLightMode, true, true);
-        }
+  });
+
+  if (!Object.keys(lightColors).length) {
+    console.warn("No variables found matching tr-color-vars attribute value");
+    return;
+  }
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  function setColors(colorObject, animate) {
+    if (typeof gsap !== "undefined" && animate) {
+      gsap.to(htmlElement, {
+        ...colorObject,
+        duration: colorModeDuration,
+        ease: colorModeEase,
       });
-    }, {
-      root: null, // observing for viewport
-      rootMargin: '0px',
-      threshold: 0.5 // adjust as needed
-    });
-  
-    // Start observing
-    document.querySelectorAll('[tr-color-scroll-toggle]').forEach(el => {
-      observer.observe(el);
+    } else {
+      Object.keys(colorObject).forEach(function (key) {
+        htmlElement.style.setProperty(key, colorObject[key]);
+      });
+    }
+  }
+
+  function goDark(dark, animate) {
+    if (dark) {
+      localStorage.setItem("dark-mode", "true");
+      htmlElement.classList.add("dark-mode");
+      setColors(darkColors, animate);
+      togglePressed = "true";
+    } else {
+      localStorage.setItem("dark-mode", "false");
+      htmlElement.classList.remove("dark-mode");
+      setColors(lightColors, animate);
+      togglePressed = "false";
+    }
+    if (typeof toggleEl !== "undefined") {
+      toggleEl.forEach(function (element) {
+        element.setAttribute("aria-pressed", togglePressed);
+      });
+    }
+  }
+
+  function checkPreference(e) {
+    goDark(e.matches, false);
+  }
+  const colorPreference = window.matchMedia("(prefers-color-scheme: dark)");
+  colorPreference.addListener(checkPreference);
+
+  let storagePreference = localStorage.getItem("dark-mode");
+  if (storagePreference !== null) {
+    storagePreference === "true" ? goDark(true, false) : goDark(false, false);
+  } else {
+    checkPreference(colorPreference);
+  }
+
+  // Function to initialize ScrollTrigger
+  function initScrollTrigger() {
+    document.querySelectorAll("[tr-scroll-toggle]").forEach((element) => {
+      ScrollTrigger.create({
+        trigger: element,
+        start: "top center", // Adjust trigger position as needed
+        onEnter: () => goDark(true, true),
+        onLeaveBack: () => goDark(false, true),
+      });
     });
   }
-  
-  colorModeToggle();
-  
+
+  // Initialize ScrollTrigger on DOMContentLoaded
+  window.addEventListener("DOMContentLoaded", (event) => {
+    initScrollTrigger();
+  });
+}
+
+colorModeScrollToggle();
