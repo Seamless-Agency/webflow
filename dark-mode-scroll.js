2eaa7323416de6c603b7c804d3d5d082df153548
Jim Ebbelaar <jimebbelaar@Jims-Mac-Studio.local>
2023-12-13 11:38:15 +0100

    scroll update

--- dark-mode-scroll.js
+++ dark-mode-scroll.js
@@ -1,115 +1,74 @@
-function colorModeScrollToggle() {
-  function attr(defaultVal, attrVal) {
-    const defaultValType = typeof defaultVal;
-    if (typeof attrVal !== "string" || attrVal.trim() === "") return defaultVal;
-    if (attrVal === "true" && defaultValType === "boolean") return true;
-    if (attrVal === "false" && defaultValType === "boolean") return false;
-    if (isNaN(attrVal) && defaultValType === "string") return attrVal;
-    if (!isNaN(attrVal) && defaultValType === "number") return +attrVal;
-    return defaultVal;
-  }
+/**
+ * Combined Script: Color Mode Toggle on Scroll
+ * Integrates color mode toggle from File 1 with scroll trigger from File 2
+ */
 
+// Part of File 1: Color Mode Toggle Functionality
+function colorModeToggle(dark, animate) {
   const htmlElement = document.documentElement;
-  const computed = getComputedStyle(htmlElement);
-  let toggleEl;
-  let togglePressed = "false";
-
   const scriptTag = document.querySelector("[tr-color-vars]");
-  if (!scriptTag) {
-    console.warn("Script tag with tr-color-vars attribute not found");
-    return;
-  }
-
+  const computed = getComputedStyle(htmlElement);
   let colorModeDuration = attr(0.5, scriptTag.getAttribute("duration"));
   let colorModeEase = attr("power1.out", scriptTag.getAttribute("ease"));
-
   const cssVariables = scriptTag.getAttribute("tr-color-vars");
-  if (!cssVariables.length) {
-    console.warn("Value of tr-color-vars attribute not found");
-    return;
-  }
 
-  let lightColors = {};
-  let darkColors = {};
+  let lightColors = {}, darkColors = {};
   cssVariables.split(",").forEach(function (item) {
-    let lightValue = computed.getPropertyValue(`--color--${item}`);
-    let darkValue = computed.getPropertyValue(`--dark--${item}`);
-    if (lightValue.length) {
-      if (!darkValue.length) darkValue = lightValue;
-      lightColors[`--color--${item}`] = lightValue;
-      darkColors[`--color--${item}`] = darkValue;
-    }
+      let lightValue = computed.getPropertyValue(`--color--${item}`);
+      let darkValue = computed.getPropertyValue(`--dark--${item}`);
+      if (lightValue.length) {
+          if (!darkValue.length) darkValue = lightValue;
+          lightColors[`--color--${item}`] = lightValue;
+          darkColors[`--color--${item}`] = darkValue;
+      }
   });
-
-  if (!Object.keys(lightColors).length) {
-    console.warn("No variables found matching tr-color-vars attribute value");
-    return;
-  }
-
-  gsap.registerPlugin(ScrollTrigger);
 
   function setColors(colorObject, animate) {
-    if (typeof gsap !== "undefined" && animate) {
-      gsap.to(htmlElement, {
-        ...colorObject,
-        duration: colorModeDuration,
-        ease: colorModeEase,
-      });
-    } else {
-      Object.keys(colorObject).forEach(function (key) {
-        htmlElement.style.setProperty(key, colorObject[key]);
-      });
-    }
+      if (typeof gsap !== "undefined" && animate) {
+          gsap.to(htmlElement, {
+              ...colorObject,
+              duration: colorModeDuration,
+              ease: colorModeEase,
+          });
+      } else {
+          Object.keys(colorObject).forEach(function (key) {
+              htmlElement.style.setProperty(key, colorObject[key]);
+          });
+      }
   }
 
-  function goDark(dark, animate) {
-    if (dark) {
-      localStorage.setItem("dark-mode", "true");
-      htmlElement.classList.add("dark-mode");
+  if (dark) {
       setColors(darkColors, animate);
-      togglePressed = "true";
-    } else {
-      localStorage.setItem("dark-mode", "false");
-      htmlElement.classList.remove("dark-mode");
-      setColors(lightColors, animate);
-      togglePressed = "false";
-    }
-    if (typeof toggleEl !== "undefined") {
-      toggleEl.forEach(function (element) {
-        element.setAttribute("aria-pressed", togglePressed);
-      });
-    }
-  }
-
-  function checkPreference(e) {
-    goDark(e.matches, false);
-  }
-  const colorPreference = window.matchMedia("(prefers-color-scheme: dark)");
-  colorPreference.addListener(checkPreference);
-
-  let storagePreference = localStorage.getItem("dark-mode");
-  if (storagePreference !== null) {
-    storagePreference === "true" ? goDark(true, false) : goDark(false, false);
   } else {
-    checkPreference(colorPreference);
+      setColors(lightColors, animate);
   }
-
-  // Function to initialize ScrollTrigger
-  function initScrollTrigger() {
-    document.querySelectorAll("[tr-scroll-toggle]").forEach((element) => {
-      ScrollTrigger.create({
-        trigger: element,
-        start: "top center", // Adjust trigger position as needed
-        onEnter: () => goDark(true, true),
-        onLeaveBack: () => goDark(false, true),
-      });
-    });
-  }
-
-  // Initialize ScrollTrigger on DOMContentLoaded
-  window.addEventListener("DOMContentLoaded", (event) => {
-    initScrollTrigger();
-  });
 }
 
-colorModeScrollToggle();
+// Helper function to parse attributes
+function attr(defaultVal, attrVal) {
+  const defaultValType = typeof defaultVal;
+  if (typeof attrVal !== "string" || attrVal.trim() === "") return defaultVal;
+  if (attrVal === "true" && defaultValType === "boolean") return true;
+  if (attrVal === "false" && defaultValType === "boolean") return false;
+  if (isNaN(attrVal) && defaultValType === "string") return attrVal;
+  if (!isNaN(attrVal) && defaultValType === "number") return +attrVal;
+  return defaultVal;
+}
+
+// Part of File 2: Scroll Trigger Integration
+window.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  // Add scroll trigger for each section with color mode toggle
+  document.querySelectorAll("[colorscroll-mode]").forEach(section => {
+      const modeIndex = +section.getAttribute("colorscroll-mode");
+
+      ScrollTrigger.create({
+          trigger: section,
+          start: "top center",
+          end: "bottom center",
+          onEnter: () => colorModeToggle(modeIndex % 2 === 0, true),
+          onLeaveBack: () => colorModeToggle(modeIndex % 2 !== 0, true)
+      });
+  });
+});
