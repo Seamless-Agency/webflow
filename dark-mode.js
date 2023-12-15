762613d7e22f36f69d804ff2352042f54ab1a978
Jim Ebbelaar <jimebbelaar@Jims-Mac-Studio.local>
2023-12-15 17:35:34 +0100

    scroll v2

---
+++ dark-mode.js
@@ -0,0 +1,135 @@
+// Define htmlElement globally
+const htmlElement = document.documentElement;
+
+function colorModeToggle() {
+  // Utility function to parse attributes
+  function attr(defaultVal, attrVal) {
+    const defaultValType = typeof defaultVal;
+    if (typeof attrVal !== "string" || attrVal.trim() === "") return defaultVal;
+    if (attrVal === "true" && defaultValType === "boolean") return true;
+    if (attrVal === "false" && defaultValType === "boolean") return false;
+    if (isNaN(attrVal) && defaultValType === "string") return attrVal;
+    if (!isNaN(attrVal) && defaultValType === "number") return +attrVal;
+    return defaultVal;
+  }
+
+  const htmlElement = document.documentElement;
+  const computed = getComputedStyle(htmlElement);
+  let toggleEl;
+  let togglePressed = "false";
+
+  const scriptTag = document.querySelector("[tr-color-vars]");
+  if (!scriptTag) {
+    console.warn("Script tag with tr-color-vars attribute not found");
+    return;
+  }
+
+  let colorModeDuration = attr(0.5, scriptTag.getAttribute("duration"));
+  let colorModeEase = attr("power1.out", scriptTag.getAttribute("ease"));
+
+  const cssVariables = scriptTag.getAttribute("tr-color-vars");
+  if (!cssVariables.length) {
+    console.warn("Value of tr-color-vars attribute not found");
+    return;
+  }
+
+  let lightColors = {};
+  let darkColors = {};
+  cssVariables.split(",").forEach(function (item) {
+    let lightValue = computed.getPropertyValue(`--color--${item}`);
+    let darkValue = computed.getPropertyValue(`--dark--${item}`);
+    if (lightValue.length) {
+      if (!darkValue.length) darkValue = lightValue;
+      lightColors[`--color--${item}`] = lightValue;
+      darkColors[`--color--${item}`] = darkValue;
+    }
+  });
+
+  if (!Object.keys(lightColors).length) {
+    console.warn("No variables found matching tr-color-vars attribute value");
+    return;
+  }
+
+  function setColors(colorObject, animate) {
+    if (typeof gsap !== "undefined" && animate) {
+      gsap.to(htmlElement, {
+        ...colorObject,
+        duration: colorModeDuration,
+        ease: colorModeEase,
+      });
+    } else {
+      Object.keys(colorObject).forEach(function (key) {
+        htmlElement.style.setProperty(key, colorObject[key]);
+      });
+    }
+  }
+
+  function goDark(dark, animate) {
+    if (dark) {
+      htmlElement.classList.add("dark-mode");
+      setColors(darkColors, animate);
+      togglePressed = "true";
+    } else {
+      htmlElement.classList.remove("dark-mode");
+      setColors(lightColors, animate);
+      togglePressed = "false";
+    }
+    if (typeof toggleEl !== "undefined") {
+      toggleEl.forEach(function (element) {
+        element.setAttribute("aria-pressed", togglePressed);
+      });
+    }
+  }
+
+  function checkPreference(e) {
+    goDark(e.matches, false);
+  }
+  const colorPreference = window.matchMedia("(prefers-color-scheme: dark)");
+  colorPreference.addListener(checkPreference);
+
+  if (storagePreference !== null) {
+    storagePreference === "true" ? goDark(true, false) : goDark(false, false);
+  } else {
+    checkPreference(colorPreference);
+  }
+
+  window.addEventListener("DOMContentLoaded", (event) => {
+    toggleEl = document.querySelectorAll("[tr-color-toggle]");
+    toggleEl.forEach(function (element) {
+      element.setAttribute("aria-label", "View Dark Mode");
+      element.setAttribute("role", "button");
+      element.setAttribute("aria-pressed", togglePressed);
+    });
+    toggleEl.forEach(function (element) {
+      element.addEventListener("click", function () {
+        let darkClass = htmlElement.classList.contains("dark-mode");
+        darkClass ? goDark(false, true) : goDark(true, true);
+      });
+    });
+  });
+  return goDark;
+}
+
+// Initialize colorModeToggle and get a reference to goDark
+const goDark = colorModeToggle();
+
+// Scroll-triggered color mode toggle
+window.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  function toggleColorModeOnScroll() {
+    const isCurrentlyDarkMode = htmlElement.classList.contains("dark-mode");
+    goDark(!isCurrentlyDarkMode, true);
+  }
+
+  document.querySelectorAll("[colorscroll-mode]").forEach((section) => {
+    ScrollTrigger.create({
+      trigger: section,
+      start: "top center",
+      end: "bottom center",
+      onEnter: toggleColorModeOnScroll,
+      onLeave: toggleColorModeOnScroll,
+      onLeaveBack: toggleColorModeOnScroll,
+    });
+  });
+});
